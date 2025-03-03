2928e11e0b4f6ede9b6f844ca1bda7ba1abdb854
Anne Nabwire <nabanne96@gmail.com>
2025-03-03 14:43:42 +0300

    handled fetch data from db and display it on website

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function PlantCard() {
+function PlantCard({plants}) {
+  
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={plants.image || "https://via.placeholder.com/400"} alt={plants.name} />
+      <h4>{plants.name}</h4>
+      <p>Price: {plants.price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
+
 function PlantList() {
+  const [plantsl, setPlantsl]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then((r)=>r.json())
+    .then((data)=>setPlantsl(data))
+    .catch((error)=>{
+      console.error('Error updating Item',error)
+    })
+  },[])
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantsl.map((plant)=>(
+      <PlantCard key={plant.id} plants={plant}/>
+    ))}</ul>
   );
 }
 
